f5248c16835733a6c85b5bb4fc01b17472646091
d_van <117540976+dinhvan2@users.noreply.github.com>
2023-04-09 22:19:31 +0700

    install husky

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,8 @@
 const newsRouter = require('./news');
 const SiteRouter = require('./site');
 function route(app) {
-  app.use('/news', newsRouter)
-    app.use('/', SiteRouter)
+  app.use('/news', newsRouter);
+  app.use('/', SiteRouter);
 }
 
 module.exports = route;
