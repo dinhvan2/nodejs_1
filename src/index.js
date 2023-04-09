ca9e9ef17bc2d866eef54bf333e3a67ea2f0c758
d_van <117540976+dinhvan2@users.noreply.github.com>
2023-04-09 22:07:37 +0700

    update

--- src/index.js
+++ src/index.js
@@ -1,24 +1,30 @@
-const path = require('path')
-const express = require('express')
-const app = express()
-const port = 3000
-const morgan = require('morgan')
+const path = require('path');
+const express = require('express');
+const app = express();
+const port = 3000;
 
-const { engine } = require ('express-handlebars');
+const route = require('./routes');
 
-app.use(express.static(path.join(__dirname, 'public'))) //static file
-app.use(morgan('combined'))
+const morgan = require('morgan');
 
-app.engine('.hbs', engine({extname: '.hbs'})); // config .handlebars
+const { engine } = require('express-handlebars');
+
+app.use(
+  express.urlencoded({
+    extended: true,
+  }),
+);
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public'))); //static file
+// app.use(morgan('combined'))
+
+app.engine('.hbs', engine({ extname: '.hbs' })); // config .handlebars
 app.set('view engine', '.hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
-app.get('/', (req, res) => {
-  res.render('home');
-})
 
-app.get('/news', (req, res) => {
-  res.render('news');
-})
+//Routes init
+route(app);
+
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
