fd56945da1db5881d150368c9df9cc1d360d7a30
d_van <117540976+dinhvan2@users.noreply.github.com>
2023-04-09 22:23:26 +0700

    second, husky

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,7 @@ const newsRouter = require('./news');
 const SiteRouter = require('./site');
 function route(app) {
   app.use('/news', newsRouter);
-      app.use('/', SiteRouter);
+  app.use('/', SiteRouter);
 }
 
 module.exports = route;
